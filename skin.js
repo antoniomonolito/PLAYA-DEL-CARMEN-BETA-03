d71ab4bb3948bea5b7ed3c838bd378ce13c52e62
antoniomonolito <69054505+antoniomonolito@users.noreply.github.com>
2020-10-23 11:43:36 -0500

    sexto

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.4/17319
 // Filename: PLAYA.ggsk
-// Generated 2020-10-23T11:36:43
+// Generated 2020-10-23T11:41:39
 
 function pano2vrSkin(player,base) {
 	player.addVariable('HT_ANI', 2, false);
